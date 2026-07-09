055952059423cffd9b158a34f5fc1b53cdebfd7c
Roberto Almanza <almanzanr@gmail.com>
2026-07-09 11:48:23 -0600

    se agregan endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import servicesRoutes from './routes/services.routes.js';
+import clientesRoutes from './routes/clientes.routes.js';
+import reservasRoutes from './routes/reservas.routes.js';
+import mesasRoutes from './routes/mesas.routes.js';
 
 export const app = express();
 app.use(express.json());
 app.use('/services', servicesRoutes);
+app.use('/clientes', clientesRoutes);
+app.use('/reservas', reservasRoutes);
+app.use('/mesas', mesasRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).json({
